dd6d7649f9f63aca519e7cb40d1c7dd7cd95dfa0
sriram-ponangi <sriram-ponangi@users.noreply.github.com>
2024-01-18 18:31:05 -0400

    Initializing the four demo spring boot projects

---
+++ demo-ms-four/src/main/java/com/example/demo/ms/four/DemoMsFourApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo.ms.four;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMsFourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMsFourApplication.class, args);
+	}
+
+}
---
+++ demo-ms-three/src/test/java/com/example/demo/ms/three/DemoMsThreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo.ms.three;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMsThreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
